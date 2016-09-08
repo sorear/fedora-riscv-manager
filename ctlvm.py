21d889254255682f73fd97044c5f384414199a7f
Stefan O'Rear <sorear2@gmail.com>
2016-09-08 07:25:17 +0000

    Changes after use I

--- ctlvm.py
+++ ctlvm.py
@@ -23,7 +23,7 @@ def safeumount(dir, file):
 
 def inspect(dir):
     safemount(dir, 'rootfs')
-    subprocess.run(['bash'])
+    subprocess.run(['bash'], cwd=dir + '/mountdir')
     safeumount(dir, 'rootfs')
 
 def injectinit(dir, text):
@@ -31,8 +31,9 @@ def injectinit(dir, text):
     safemount(dir, 'scratch')
     with open(dir + '/mountdir/init', 'w') as init:
         uxtime = datetime.datetime.now().isoformat()
+        columns, lines = os.get_terminal_size()
         init.write("#!/bin/bash\n")
-        init.write("date --set " + uxtime + "\n")
+        init.write("date --set " + uxtime + "; export LINES=" + str(lines) + "; export COLUMNS=" + str(columns) + "\n")
         init.write("""
 PS4='stage3$ '
 mount.static -o remount,rw /
@@ -63,7 +64,14 @@ set -x
         init.write(text + "\n")
         init.write("""
 set +x
-mount.static -n -o remount,ro /
+sync
+while ! mount.static -n -o remount,ro /; do
+    ps -elf
+    sleep 2
+    kill -- -1
+    sleep 2
+    kill -9 -- -1
+done
 sync
 echo This message indicates that the VM shut down correctly.
 poweroff
@@ -74,7 +82,7 @@ def runqemu(dir, timeout, no_input):
     input = None
     if no_input:
         input = subprocess.PIPE
-    with subprocess.Popen(['script','-c','timeout -s9 ' + str(timeout) + ' qemu-system-riscv -m 2G -kernel /usr/bin/bbl -append vmlinux -drive file=scratch,format=raw -nographic','lastlog'], cwd=dir, stdin=input) as p:
+    with subprocess.Popen(['script','-a','-c','timeout -s9 ' + str(timeout) + ' qemu-system-riscv -m 2G -kernel /usr/bin/bbl -append vmlinux -drive file=scratch,format=raw -nographic','lastlog'], cwd=dir, stdin=input) as p:
         p.wait()
     with open(dir + '/lastlog', 'r') as f:
         log = f.read()
@@ -84,12 +92,34 @@ def runqemu(dir, timeout, no_input):
         raise Exception("VM did not shut down cleanly")
 
 def interact(dir):
-    injectinit(dir, "script /dev/null")
+    injectinit(dir, "bash")
     runqemu(dir, 9999999, False)
 
 def runshort(dir, timeout, cmd):
     injectinit(dir, cmd)
     runqemu(dir, timeout, True)
+
+def autobuild(dir, srpm):
+    safemount(dir, 'rootfs')
+    subprocess.run(['sudo', 'cp', srpm, dir + '/mountdir/input.srpm'])
+    safeumount(dir, 'rootfs')
+    runshort(dir, 99999, 'rm -rf /rpmbuild && rpm -i /input.srpm && script -c "rpmbuild -ba --define \\"debug_package %{nil}\\" --nodeps /rpmbuild/SPECS/*.spec"')
+    getrpms(dir)
+
+def autobuild_edit(dir):
+    runshort(dir, 99999, 'rpmbuild -ba --define "debug_package %{nil}" --nodeps /rpmbuild/SPECS/*.spec')
+    getrpms(dir)
+
+def getrpms(dir):
+    safemount(dir, 'rootfs')
+    subprocess.run(['sudo', 'sh', '-c', 'cp '+dir+'/mountdir/rpmbuild/RPMS/*/* '+dir+'/mountdir/rpmbuild/SRPMS/* '+dir])
+    safeumount(dir, 'rootfs')
+
+def injectrpm(dir, *rpmglob):
+    safemount(dir, 'rootfs')
+    subprocess.run(['sudo', 'sh', '-c', 'cp '+' '.join(rpmglob)+' '+dir+'/mountdir/tmp/'])
+    safeumount(dir, 'rootfs')
+    runshort(dir, 99999, '(set +x; for x in $(rpm -qa); do rm -f /tmp/$x.rpm; done); rpm -Uvh --nodeps /tmp/*.rpm && rm /tmp/*.rpm')
 
 if __name__ == "__main__":
     globals()[sys.argv[1]](*sys.argv[2:])
